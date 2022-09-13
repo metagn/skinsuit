24ba83c695ed1ecfddbf631ae5ec711eac2725ae
hlaaftana <10591326+metagn@users.noreply.github.com>
2022-09-13 09:19:28 +0300

    make conditional test backend specific

--- tests/test_conditional.nim
+++ tests/test_conditional.nim
@@ -1,62 +1,63 @@
-import variantsugar/conditional
+when not defined(js) and not defined(nimscript):
+  import variantsugar/conditional
 
-conditional:
-  type Foo = ref object
-    num: int
-    case branch: _ # type has to be _, name can also be _
-    of Odd, num mod 2 == 1: # branch names can also be _
-      name: string
-    of DoubleEven, num mod 4 == 0:
-      a*, b: int
-    of Even:
-      a: int
+  conditional:
+    type Foo = ref object
+      num: int
+      case branch: _ # type has to be _, name can also be _
+      of Odd, num mod 2 == 1: # branch names can also be _
+        name: string
+      of DoubleEven, num mod 4 == 0:
+        a*, b: int
+      of Even:
+        a: int
 
-block:
-  var f = Foo(num: 1)
-  f.name = "abc"
-  doAssert f.name == "abc"
-  doAssert f.branch == Odd
-  f.num = 16
-  f.resetBranch()
-  doAssert f.a == 0
-  doAssert f.b == 0
-  f.a = 5
-  doAssert f.a == 5
-  f.b = 7
-  doAssert f.b == 7
-  doAssert f.branch == DoubleEven
-  f.num = 2
-  doAssert f.branch == Even
-  doAssert f.a == 5
-  f.resetBranch()
-  doAssert f.a == 0
+  block:
+    var f = Foo(num: 1)
+    f.name = "abc"
+    doAssert f.name == "abc"
+    doAssert f.branch == Odd
+    f.num = 16
+    f.resetBranch()
+    doAssert f.a == 0
+    doAssert f.b == 0
+    f.a = 5
+    doAssert f.a == 5
+    f.b = 7
+    doAssert f.b == 7
+    doAssert f.branch == DoubleEven
+    f.num = 2
+    doAssert f.branch == Even
+    doAssert f.a == 5
+    f.resetBranch()
+    doAssert f.a == 0
 
-block:
-  type Foo {.conditional.} = ref object
-    num: int
-    case branch: _ # type has to be _, name can also be _
-    of Odd, num mod 2 == 1: # branch names can also be _
-      name: string
-    of DoubleEven, num mod 4 == 0:
-      a, b: int
-    of Even:
-      a: int
-  var f = Foo(num: 1)
-  f.name = "abc"
-  doAssert f.name == "abc"
-  doAssert f.branch == Odd
-  f.num = 16
-  f.resetBranch()
-  doAssert f.a == 0
-  doAssert f.b == 0
-  f.a = 5
-  doAssert f.a == 5
-  f.b = 7
-  doAssert f.b == 7
-  doAssert f.branch == DoubleEven
-  f.num = 2
-  doAssert f.branch == Even
-  doAssert f.a == 5
-  f.resetBranch()
-  doAssert f.a == 0
+  block:
+    type Foo {.conditional.} = ref object
+      num: int
+      case branch: _ # type has to be _, name can also be _
+      of Odd, num mod 2 == 1: # branch names can also be _
+        name: string
+      of DoubleEven, num mod 4 == 0:
+        a, b: int
+      of Even:
+        a: int
+    var f = Foo(num: 1)
+    f.name = "abc"
+    doAssert f.name == "abc"
+    doAssert f.branch == Odd
+    f.num = 16
+    f.resetBranch()
+    doAssert f.a == 0
+    doAssert f.b == 0
+    f.a = 5
+    doAssert f.a == 5
+    f.b = 7
+    doAssert f.b == 7
+    doAssert f.branch == DoubleEven
+    f.num = 2
+    doAssert f.branch == Even
+    doAssert f.a == 5
+    f.resetBranch()
+    doAssert f.a == 0
 
