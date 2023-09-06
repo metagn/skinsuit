feb267a5a18c74e3b2b48028c48cf1232fae3ade
metagn <10591326+metagn@users.noreply.github.com>
2023-09-06 11:40:16 +0300

    statement version of `equals` for existing types

--- src/skinsuit/equals.nim
+++ src/skinsuit/equals.nim
@@ -15,55 +15,87 @@ runnableExamples:
 
 import macros, private/utils
 
+proc equalsProc(typeName, objectNode: NimNode, doExport, ptrLike: bool): NimNode =
+  proc generateEquals(sl: NimNode, field: NimNode) =
+    case field.kind
+    of nnkIdentDefs:
+      for f in field[0 .. ^3]:
+        sl.add quote do:
+          if a.`f` != b.`f`:
+            return false
+    of nnkRecCase:
+      let kf = field[0][0]
+      sl.add quote do:
+        if a.`kf` != b.`kf`:
+          return false
+      let cs = newTree(nnkCaseStmt, newDotExpr(ident"a", kf))
+      for b in field[1 .. ^1]:
+        let branch = copy b
+        branch[^1] = newStmtList()
+        for r in b[^1]:
+          generateEquals(branch[^1], r)
+        cs.add(branch)
+      sl.add(cs)
+    of nnkRecWhen:
+      let ws = newTree(nnkWhenStmt)
+      for b in field:
+        let branch = copy b
+        branch[^1] = newStmtList()
+        for r in b[^1]:
+          generateEquals(branch[^1], r)
+        ws.add(branch)
+      sl.add(ws)
+    else: discard
+  let equalsBody = newStmtList()
+  if ptrLike:
+    equalsBody.add quote do:
+      if cast[pointer](a) == cast[pointer](b):
+        return true
+      if a.isNil or b.isNil:
+        return false
+  for r in objectNode[^1]:
+    generateEquals(equalsBody, r)
+  equalsBody.add(newTree(nnkReturnStmt, ident"true"))
+  newProc(
+    name = ident"==".exportIf(doExport),
+    params = [ident"bool", newTree(nnkIdentDefs, ident"a", ident"b", typeName, newEmptyNode())],
+    body = equalsBody,
+    pragmas = newTree(nnkPragma, ident"used")
+  )
+
 proc patchTypeSection(typeSec: NimNode, poststmts: var seq[NimNode]) =
   for td in typeSec:
     var objectNode = td[^1]
+    var ptrLike = false
     while objectNode.kind in {nnkRefTy, nnkPtrTy}:
       objectNode = objectNode[0]
+      ptrLike = true
     if objectNode.kind == nnkObjectTy:
       let doExport = td[0].isNodeExported
       let typeName = td[0].realBasename
-      # ==
-      proc generateEquals(sl: NimNode, field: NimNode) =
-        case field.kind
-        of nnkIdentDefs:
-          for f in field[0 .. ^3]:
-            sl.add quote do:
-              if a.`f` != b.`f`:
-                return false
-        of nnkRecCase:
-          let kf = field[0][0]
-          sl.add quote do:
-            if a.`kf` != b.`kf`:
-              return false
-          let cs = newTree(nnkCaseStmt, newDotExpr(ident"a", kf))
-          for b in field[1 .. ^1]:
-            let branch = copy b
-            branch[^1] = newStmtList()
-            for r in b[^1]:
-              generateEquals(branch[^1], r)
-            cs.add(branch)
-          sl.add(cs)
-        of nnkRecWhen:
-          let ws = newTree(nnkWhenStmt)
-          for b in field:
-            let branch = copy b
-            branch[^1] = newStmtList()
-            for r in b[^1]:
-              generateEquals(branch[^1], r)
-            ws.add(branch)
-          sl.add(ws)
-        else: discard
-      let equalsBody = newStmtList()
-      for r in objectNode[^1]:
-        generateEquals(equalsBody, r)
-      equalsBody.add(newTree(nnkReturnStmt, ident"true"))
-      poststmts.add(newProc(
-        name = ident"==".exportIf(doExport),
-        params = [ident"bool", newTree(nnkIdentDefs, ident"a", ident"b", ident(typeName), newEmptyNode())],
-        body = equalsBody,
-        pragmas = newTree(nnkPragma, ident"used")
-      ))
+      poststmts.add(equalsProc(ident(typeName), objectNode, doExport, ptrLike))
+
+macro equalsExistingType(t, T: typed, exported: static bool) =
+  var objectNode = t.getTypeImpl
+  var ptrLike = false
+  while true:
+    case objectNode.kind
+    of nnkRefTy, nnkPtrTy:
+      objectNode = objectNode[0]
+      ptrLike = true
+    of nnkSym, nnkBracketExpr: discard
+    else: break
+    objectNode = objectNode.getTypeImpl
+  if objectNode.kind == nnkObjectTy:
+    result = equalsProc(T, objectNode, exported, ptrLike)
 
 macro equals*(body) =
-  result = applyTypeMacro(body, patchTypeSection)
+  if body.kind in {nnkTypeDef, nnkTypeSection, nnkStmtList}:
+    result = applyTypeMacro(body, patchTypeSection)
+  else:
+    var body = body
+    var exported = false
+    if body.kind == nnkPrefix and body[0].eqIdent"*":
+      body = body[1]
+      exported = true
+    result = newCall(bindSym"equalsExistingType", newCall(bindSym"default", body), body, newLit exported)
--- tests/test_equals.nim
+++ tests/test_equals.nim
@@ -12,3 +12,27 @@ block:
   doAssert Foo(name: "abc", kind: false, a: 1) == Foo(name: "abc", kind: false, a: 1)
   doAssert Foo(name: "abc", kind: false, a: 1) != Foo(name: "abc", kind: true, b: 1)
   doAssert Foo(name: "abc", kind: false, a: 1) != Foo(name: "def", kind: false, a: 1)
+
+block:
+  type
+    Bar = ref object
+      x: Foo
+    Foo = object
+      name: string
+      bar: Bar
+      case kind: bool
+      of false:
+        a: int
+      of true:
+        b: float
+
+  proc `==`(a, b: Foo): bool
+  
+  equals Bar
+  equals Foo
+  
+  doAssert Foo(name: "abc", kind: false, a: 1) == Foo(name: "abc", kind: false, a: 1)
+  doAssert Foo(name: "abc", kind: false, a: 1) != Foo(name: "abc", kind: true, b: 1)
+  doAssert Foo(name: "abc", kind: false, a: 1) != Foo(name: "def", kind: false, a: 1)
+  doAssert Bar(x: Foo(name: "abc")) == Bar(x: Foo(name: "abc"))
+  doAssert Bar(x: Foo(name: "abc")) != Bar(x: Foo(name: "def"))
